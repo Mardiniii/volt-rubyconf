ef554d9cc8385ab75b2656ef8d9311fd34ed0be0
mardiniii <sezama03@hotmail.com>
2015-10-18 18:08:25 -0500

    Get current chat into main controller

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -16,6 +16,11 @@ module Main
     end
 
     private
+      def current_chat
+        current_index = (params._index) || 0).to_i
+        store.chats[current_index]
+      end
+
       def reset_message
         page._message_body = ''
       end
